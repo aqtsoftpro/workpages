e529fecbae82661ad307e57cc21892b754d84f0e
Afzaal565 <afzaalhussain565@gmail.com>
2024-02-15 14:51:31 +0500

    cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+    'allowed_origins' => ['https://wpageapp.aqtdemos.com'],
 
     'allowed_origins_patterns' => ['*'],
 
