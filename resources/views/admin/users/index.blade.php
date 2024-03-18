988fe5f31d7d87da17e16ee64a24c6ac11208910
Afzaal565 <afzaalhussain565@gmail.com>
2024-03-18 09:53:29 +0500

    some datatabel issues fixed

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,5 +1,8 @@
-@extends('layouts.app')
 
+
+
+
+@extends('layouts.app')
 @section('content')
     <div class="pagetitle">
         <h1>Total Users</h1>
@@ -16,71 +19,78 @@
     @elseif(Session::has('error'))
         <div class="alert alert-danger">{{ Session::get('error') }}</div>
     @endif
+
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Job Seekers</h5>
+                        <h5 class="card-title row">
+                            <div class="col-lg-6">
+                                Total user list
+                            </div>
 
+                        </h5>
+
+                        <!-- Table with stripped rows -->
                         <table class="table datatable">
                             <thead>
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Email</th>
-                                    <th scope="col">Actions</th>
+                                    <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($job_seekers as $job_seeker)
-                                    <tr>
-                                        <td>{{ $job_seeker->name }}</td>
-                                        <td>{{ $job_seeker->email }}</td>
-                                        <td>
-                                            <a class="mx-1 text-success"
-                                                href="{{ route('users.edit', $job_seeker->id) }}"><i class="bi bi-pen"></i>
-                                            </a>|
-                                            @if (!$job_seeker->hasRole('Super Admin'))
-                                                <a type="#" class="mx-1 text-danger" data-bs-toggle="modal"
-                                                    data-bs-target="#deleteModel-{{ $job_seeker->id }}"><i
-                                                        class="bi bi-trash"></i></a>
+                                    <td scope="row">{{ $loop->iteration }}</td>
+                                    <td>{{ $job_seeker->name }}</td>
+                                    <td>{{ $job_seeker->email }}</td>
 
-                                                <div class="modal fade" id="deleteModel-{{ $job_seeker->id }}"
-                                                    tabindex="-1">
-                                                    <div class="modal-dialog modal-dialog-centered">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title">Delete Users</h5>
-                                                                <button type="button" class="btn-close"
-                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                Are you sure You want to Delete
-                                                                <b>{{ $job_seeker->name }}</b>?
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-secondary"
-                                                                    data-bs-dismiss="modal">Close</button>
-                                                                <form id="delete-form"
-                                                                    action="{{ route('users.destroy', $job_seeker->id) }}"
-                                                                    method="POST">
-                                                                    @csrf
-                                                                    @method('DELETE')
-                                                                    <button type="submit"
-                                                                        class="btn btn-danger">Delete</button>
-                                                                </form>
-                                                            </div>
+                                    <td>
+                                        <a class="mx-1 text-success" href="{{ route('users.edit', $job_seeker->id) }}"><i
+                                                class="bi bi-pen"></i>
+                                        </a>|
+                                        @if (!$job_seeker->hasRole('Super Admin'))
+                                            <a type="#" class="mx-1 text-danger" data-bs-toggle="modal"
+                                                data-bs-target="#deleteModel-{{ $job_seeker->id }}"><i
+                                                    class="bi bi-trash"></i></a>
+
+                                            <div class="modal fade" id="deleteModel-{{ $job_seeker->id }}" tabindex="-1">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title">Delete Users</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Are you sure You want to Delete
+                                                            <b>{{ $job_seeker->name }}</b>?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary"
+                                                                data-bs-dismiss="modal">Close</button>
+                                                            <form id="delete-form"
+                                                                action="{{ route('users.destroy', $job_seeker->id) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <button type="submit"
+                                                                    class="btn btn-danger">Delete</button>
+                                                            </form>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            @endif
-
-                                        </td>
-                                    <tr>
+                                            </div>
+                                        @endif
+                                    </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
+                        <!-- End Table with stripped rows -->
                     </div>
                 </div>
             </div>
