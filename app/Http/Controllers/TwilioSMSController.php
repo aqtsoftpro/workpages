8c3869fe9bd89bbcfeee3b2fca17bb757bea1c45
jamshed45 <jamshed76@gmail.com>
2025-02-21 17:50:43 +0500

    WorkPages Admin Update

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -114,14 +114,8 @@ class TwilioSMSController extends Controller
                 $verificationUrl = rtrim($customBaseUrl). 'user/dashboard';
                 $subject = $request->subject;
                 $To = $user->email;
-
-                Mail::html($originalContent, function ($message) use ($To, $subject) {
-                    $message->to($To)
-                            ->subject($subject);
-                });
-
-                // $email = new MultiPurposeEmail($subject, $originalContent, $verificationUrl);
-                // Mail::to($To)->send($email);
+                $email = new MultiPurposeEmail($subject, $originalContent, $verificationUrl);
+                Mail::to($To)->send($email);
 
             }
             return response()->json(['status' => 'success', 'message' => 'Email successfully sent!']);
