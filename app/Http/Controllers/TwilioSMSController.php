36811a0f03d46c20f3fd3b7bc2793130d0339dae
jamshed45 <jamshed76@gmail.com>
2025-02-24 15:52:23 +0500

    WorkPages Admin Bulk Email

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -96,8 +96,6 @@ class TwilioSMSController extends Controller
         $user_id = $request->user_id;
         $userIds = explode(',', $user_id);
 
-        print_r($userIds);
-
         if($userIds && $company){
             foreach ($userIds as $id) {
                 if($id != '')
@@ -106,7 +104,7 @@ class TwilioSMSController extends Controller
 
                     $email_templates  = new EmailTemplateController();
                     $get_template = $email_templates->get_template('job-seeker-email');
-                    echo $originalContent = $get_template['desc'];
+                    $originalContent = $get_template['desc'];
 
                     $email_variables = [
                         '[username]' => $user->name,
@@ -119,8 +117,6 @@ class TwilioSMSController extends Controller
                     };
 
                     // $verificationUrl = rtrim($customBaseUrl). 'user/dashboard';
-
-                    echo $originalContent;
 
                     $subject = $request->subject;
 
