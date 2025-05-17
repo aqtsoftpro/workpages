237975a60612871893dc632b700c695ffb413940
jamshed45 <jamshed76@gmail.com>
2025-05-17 17:22:11 +0500

    email update

--- app/Http/Controllers/ApiVerifyEmailController.php
+++ app/Http/Controllers/ApiVerifyEmailController.php
@@ -42,7 +42,7 @@ class ApiVerifyEmailController extends Controller
 
         if ($user) {
 
-            $customBaseUrl = env('FRONT_APP_URL', 'https://workpages.ws/');
+            $customBaseUrl = config('app.front_app_url');
             $randomString = Str::random(40);
             $expired = now()->addMinutes(60);
             $verifyMail = VerifyEmail::where('user_id', $user->id)->first();
